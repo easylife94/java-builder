5a39ebccb53ee80f60ca27cd3a0334e38e72670f
chenwei <cw961590280@hotmail.com>
2019-05-27 14:14:13 +0800

    project init

---
+++ java-builder-admin/src/main/java/indi/easylife94/javabuilder/admin/JavaBuilderAdminApplication.java
@@ -0,0 +1,13 @@
+package indi.easylife94.javabuilder.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBuilderAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBuilderAdminApplication.class, args);
+    }
+
+}
